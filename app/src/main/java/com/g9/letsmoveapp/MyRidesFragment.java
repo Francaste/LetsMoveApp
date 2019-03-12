e96b66aa00755c22df6cebb0a60dcf5e0f578676
vsiguagua <100330029@alumnos.uc3m.es>
2019-03-12 10:18:05 +0100

    Botones del menu hechos

--- app/src/main/java/com/g9/letsmoveapp/MyRidesFragment.java
+++ app/src/main/java/com/g9/letsmoveapp/MyRidesFragment.java
@@ -9,6 +9,6 @@ import android.view.ViewGroup;
 public class MyRidesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        return inflater.inflate(R.layout.fragment_myrides, container, false);
     }
 }
