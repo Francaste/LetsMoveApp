7b1a241dbb73516af014d87411d0288dac2e5116
Francaste <francastefcb@gmail.com>
2019-03-30 21:57:48 +0100

    Ya se coge adecuadamente el input stream de la petición HTTPS. Se pasa de Input Stream a String
    Falta desarrollar el método comentado para leer del json los campos con json reader

--- app/src/main/java/com/g9/letsmoveapp/TestWeatherActivity.java
+++ app/src/main/java/com/g9/letsmoveapp/TestWeatherActivity.java
@@ -12,6 +12,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpResponse;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -22,6 +23,7 @@ import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
@@ -55,20 +57,27 @@ public class TestWeatherActivity extends AppCompatActivity {
     public class AemetWeather extends AsyncTask<Object, Void, Object> {
 
         @Override
-        protected Object doInBackground(Object[] objects) {
-
-
+        protected String doInBackground(Object[] objects) {
             String CP = "28109"; // Codigo postal de Pelayos de la Presa
             String stringURL = "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/" +
                     CP + "/?api_key=" + AEMET_KEY;
 
             tv_1.setText(stringURL);
             Log.d(LOG_TAG, stringURL);
-
-
-            URL url = null;
-            BufferedInputStream is = null;
+            String json= getJSONResponse(stringURL);
+            Log.d(LOG_TAG, json);
+            //Leer campo datos del json, coger url y repetir el proceso
             JsonReader jsonReader;
+
+
+            return null;
+        }
+
+        public String getJSONResponse(String stringURL){
+            URL url = null;
+            InputStream is = null;
+            JSONObject jObj = null;
+            String json = "";
 
             try {
                 url = new URL(stringURL);
@@ -78,47 +87,117 @@ public class TestWeatherActivity extends AppCompatActivity {
 
             try {
                 if (url != null) {
+                    //Abrimos conexión HTTPS, con método GET y nos conectamos
                     HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
                     urlConnection.setRequestMethod("GET");
                     urlConnection.connect();
+                    //Sacamos el código de respuesta
+                    int responseCode = urlConnection.getResponseCode();
+                    Log.d(LOG_TAG, "The response is: " + responseCode);
 
-                    int response = urlConnection.getResponseCode();
-                    Log.d(LOG_TAG, "The response is: " + response);
-
-
-                    // Peta en esta linea ->
+                    //Leemos el input stream
                     is = new BufferedInputStream(urlConnection.getInputStream());
-
-                    Log.d(LOG_TAG, is.toString());
+                    //Convertimos inputStream a String
+                    json = streamToString(is);
                 }
             } catch (IOException ioe) {
                 System.out.println("IOException");
             }
+            return json;
+        }
+        //Convierte un InputStream a un String
+        public String streamToString(InputStream is) {
+            //Para hacer un buffer para
+            BufferedReader buffReader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();//
 
-
-            if (is != null) {
+            String responseStr = null;
+            try {
+                //Mientras que la línea que se ha leído no sea nula, sigue leyendo
+                while ((responseStr = buffReader.readLine()) != null) {
+                    sb.append(responseStr + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
                 try {
-                    InputStreamReader isReader = new InputStreamReader(is, "UTF-8");
-                    jsonReader = new JsonReader(isReader);
-
-                    BufferedReader buff = new BufferedReader(isReader);
-                    String response = buff.readLine();
-
-
-                    Log.d(LOG_TAG,response);
-
-
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
+                    //Cierra el stream
+                    is.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-
-
-            return null;
+            return sb.toString();
         }
-
+      //DESARROLLAR METODO PARA LEER CAMPOS DE JSON
+        //PRIMERO PARA SACAR DEL CAMPO DATOS LA URL Y LUEGO PARA LOS CAMPOS ÚTILES DE LA PETICION AEMET
+      /* *********************************
+        public String readDataJson(InputStream is, JsonReader jsonReader){
+            try {
+                jsonReader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+                jsonReader.beginObject();
+                while (jsonReader.hasNext()) {
+                    String name = jsonReader.nextName();
+                    // Busca la cadena "results"
+                    if (name.equals("descripcion")) {
+                        // comienza un array de objetos
+                        jsonReader.beginArray();
+                        while (jsonReader.hasNext()) {
+                            String descr="";
+                            jsonReader.beginObject();
+                            // comienza un objeto
+                            while (jsonReader.hasNext()) {
+                                name = jsonReader.nextName();
+                                if (name.equals("name")) {
+                                    // si clave "name" guarda el valor
+                                    poi.setName(jsonReader.nextString());
+                                    System.out.println("PLACE NAME:" + poi.getName());
+                                } else if (name.equals("geometry")) {
+                                    // Si clave "geometry" empieza un objeto
+                                    jsonReader.beginObject();
+                                    while (jsonReader.hasNext()) {
+                                        name = jsonReader.nextName();
+                                        if (name.equals("location")) {
+                                            // dentro de "geometry", si clave "location" empieza un objeto
+                                            jsonReader.beginObject();
+                                            while (jsonReader.hasNext()) {
+                                                name = jsonReader.nextName();
+                                                // se queda con los valores de "lat" y "long" de ese objeto
+                                                if (name.equals("lat")) {
+                                                    poi.setLatitude(jsonReader.nextString());
+                                                    System.out.println("PLACE LATITUDE:" + poi.getLatitude());
+                                                } else if (name.equals("lng")) {
+                                                    poi.setLongitude(jsonReader.nextString());
+                                                    System.out.println("PLACE LONGITUDE:" + poi.getLongitude());
+                                                } else {
+                                                    jsonReader.skipValue();
+                                                }
+                                            }
+                                            jsonReader.endObject();
+                                        } else {
+                                            jsonReader.skipValue();
+                                        }
+                                    }
+                                    jsonReader.endObject();
+                                } else{
+                                    jsonReader.skipValue();
+                                }
+                            }
+                            jsonReader.endObject();
+                            temp.add(poi);
+                        }
+                        jsonReader.endArray();
+                    } else {
+                        jsonReader.skipValue();
+                    }
+                }
+                jsonReader.endObject();
+            }
+            catch (Exception e) {
+            System.out.println("Exception");
+            return new ArrayList<GooglePlace>();
+        }
+        ******************************** */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
