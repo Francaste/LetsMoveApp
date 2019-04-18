ba299a30fb3ecfbc2dfe762f43a7afa28b6a80f9
Francaste <francastefcb@gmail.com>
2019-04-18 21:54:28 +0200

    Avances con la cámara, hay que averiguar cómo solventar el problema del Intent null que devuelve la actividad al pasar los extras(imagen)

--- app/src/main/java/com/g9/letsmoveapp/MainActivity.java
+++ app/src/main/java/com/g9/letsmoveapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.g9.letsmoveapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
@@ -15,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import java.util.ArrayList;
 
 import com.g9.letsmoveapp.DatabaseAdapter.DatabaseHelper;
@@ -63,7 +65,7 @@ public class MainActivity extends AppCompatActivity
         try {
             DatabaseAdapter dbAdapter = new DatabaseAdapter(this);
             dbAdapter.mDBHelper.onCreate(dbAdapter.mDB);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -71,101 +73,100 @@ public class MainActivity extends AppCompatActivity
     //Triple unido al a anterior línea del context del adaptador
 
 
-        @Override
-        public void onBackPressed() {
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            if (drawer.isDrawerOpen(GravityCompat.START)) {
-                drawer.closeDrawer(GravityCompat.START);
-            } else {
-                super.onBackPressed();
-            }
-        }
-
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.main, menu);
-            return true;
-        }
-
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
-
-            return super.onOptionsItemSelected(item);
-        }
-
-        @SuppressWarnings("StatementWithEmptyBody")
-        @Override
-        public boolean onNavigationItemSelected (MenuItem item){
-            // Handle navigation view item clicks here.
-            int id = item.getItemId();
-
-            FragmentManager fragmentManager = getSupportFragmentManager();
-
-
-            if (id == R.id.nav_inicio) {
-                fragmentManager.beginTransaction().replace(R.id.contenedor, new MenuFragment()).commit();
-            } else if (id == R.id.nav_perfil) {
-                fragmentManager.beginTransaction().replace(R.id.contenedor, new ProfileFragment()).commit();
-            } else if (id == R.id.nav_trayecto) {
-                fragmentManager.beginTransaction().replace(R.id.contenedor, new NewRidesFragment()).commit();
-            } else if (id == R.id.nav_viajes) {
-                fragmentManager.beginTransaction().replace(R.id.contenedor, new MyRidesFragment()).commit();
-            } else if (id == R.id.nav_notificacion) {
-                fragmentManager.beginTransaction().replace(R.id.contenedor, new NotificationFragment()).commit();
-            } else if (id == R.id.nav_puntuacion) {
-                fragmentManager.beginTransaction().replace(R.id.contenedor, new PuntuaFragment()).commit();
-            } else if (id == R.id.nav_configuracion) {
-                fragmentManager.beginTransaction().replace(R.id.contenedor, new SettingsFragment()).commit();
-            } else if (id == R.id.nav_compartir) {
-                fragmentManager.beginTransaction().replace(R.id.contenedor, new ShareFragment()).commit();
-            } else if (id == R.id.nav_faq){
-                fragmentManager.beginTransaction().replace(R.id.contenedor, new FaqFragment()).commit();
-
-            }
-
-
-
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
 
+        return super.onOptionsItemSelected(item);
+    }
 
-        public void actuales (View view){
-            Intent intent = new Intent(this, ViajesActuales.class);
-            startActivity(intent);
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+
+        if (id == R.id.nav_inicio) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new MenuFragment()).commit();
+        } else if (id == R.id.nav_perfil) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new ProfileFragment()).commit();
+        } else if (id == R.id.nav_trayecto) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new NewRidesFragment()).commit();
+        } else if (id == R.id.nav_viajes) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new MyRidesFragment()).commit();
+        } else if (id == R.id.nav_notificacion) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new NotificationFragment()).commit();
+        } else if (id == R.id.nav_puntuacion) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new PuntuaFragment()).commit();
+        } else if (id == R.id.nav_configuracion) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new SettingsFragment()).commit();
+        } else if (id == R.id.nav_compartir) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new ShareFragment()).commit();
+        } else if (id == R.id.nav_faq) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new FaqFragment()).commit();
+
         }
 
-        public void anteriores (View view){
-            Intent intent = new Intent(this, ViajesAnteriores.class);
-            startActivity(intent);
-        }
-        public void gastos (View view){
-            Intent intent = new Intent(this, GastosViajes.class);
-            startActivity(intent);
-        }
 
-        public void misCoches (View view){
-            Intent intent = new Intent(this, MisCoches.class);
-            startActivity(intent);
-        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void actuales(View view) {
+        Intent intent = new Intent(this, ViajesActuales.class);
+        startActivity(intent);
+    }
+
+    public void anteriores(View view) {
+        Intent intent = new Intent(this, ViajesAnteriores.class);
+        startActivity(intent);
+    }
+
+    public void gastos(View view) {
+        Intent intent = new Intent(this, GastosViajes.class);
+        startActivity(intent);
+    }
+
+    public void misCoches(View view) {
+        Intent intent = new Intent(this, MisCoches.class);
+        startActivity(intent);
+    }
 
 
-
-// Este metodo lanza una actividad para testear la API AEMET
+    // Este metodo lanza una actividad para testear la API AEMET
 // al pulsar el boton TEST WEATHER que esta en Mis Viajes
     public void testWeather(View view) {
         Intent intent = new Intent(this, TestWeatherActivity.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/g9/letsmoveapp/NuevoCoche.java
+++ app/src/main/java/com/g9/letsmoveapp/NuevoCoche.java
@@ -3,10 +3,14 @@ package com.g9.letsmoveapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,34 +20,34 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class NuevoCoche extends Activity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    public static final String LOG_TAG = "NuevoCoche";
-
     Button button_camera;
-    Button add_new_car;
-
     ImageView fotoCar;
+    private final static String CLAVE_RUTA_IMAGEN = "CLAVE_RUTA_IMAGEN";
+    public static final String LOG_TAG = "nuevoCoche";
+
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
 
     public void onCreate(Bundle savedInstanceState) {
         //Te deja controlar la actividad general "haciendo como que entras", coge la clase superior
-        super.onCreate(savedInstanceState); setContentView(R.layout.nuevo_coche);
-        button_camera = findViewById(R.id.cam_car);
-        add_new_car = findViewById(R.id.add_new_car);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nuevo_coche);
 
+        button_camera = findViewById(R.id.cam_car);
         fotoCar=findViewById(R.id.foto_car);
         button_camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                intentCamara(view);
-            }
-        });
-        add_new_car.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                add_car();
+                //startActivity(takePictureIntent);
+                dispatchTakePictureIntent(takePictureIntent);
             }
         });
     }
@@ -54,16 +58,9 @@ public class NuevoCoche extends Activity {
     }
 */
 
-    public void add_car() {
+    public void add_car(View view) {
         EditText editText = (EditText) findViewById(R.id.nombre_nuevo_coche);
         String message = "Coche: " + editText.getText().toString() + " añadido correctamente";
-        try {
-            DatabaseAdapter dbAdapter=new DatabaseAdapter(this);
-            CarsDataModel carsDataModel=new CarsDataModel();
-            dbAdapter.addCarsData(carsDataModel);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
         finish();
 
@@ -72,25 +69,85 @@ public class NuevoCoche extends Activity {
         //Intent intent = new Intent(this, MisCoches.class);
         //startActivity(intent);
     }
-    /*
-    public void onClick(View view) {
-        intentCamara();
 
-    }
-    */
-// en codelabs era private
+/*en codelabs era private
+  Este es el método que estaba ayer cuando la cámara funcionaba sin problemas
     public void intentCamara(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-    }
+    }*/
 
+    //Este es el método que estaba ayer cuando la cámara funcionaba sin problemas pero no se usaba
+    // de por sí en ningún sitio y ahora con la nueva, es el código que da problemas
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Log.d(LOG_TAG, "Data: "+data.toString());
+            // Continue only if the File was successfully created
+            Uri photoUri = data.getData();
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Bitmap imageBitmap = null;
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             fotoCar.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+    //Guardar la foto en la galería o donde sea, hemos importao File, IOException, SimpleDateFormat
+    //Y Environment, que se ha importado más arriba con los otros import (URI y FileProvider
+    // también están arriba)
+
+
+//Esto es nuevo
+    String currentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+//Nuevo
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    //Una vez que tenemos un método para crear ficheros, lanzamos la cámara
+
+    //Nuevo
+    private void dispatchTakePictureIntent(Intent takePictureIntent) {
+        Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                System.out.println("Exception");
+                ex.printStackTrace();
+            }
+           // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.g9.letsmoveapp",
+                        photoFile);
+                photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(photoIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
