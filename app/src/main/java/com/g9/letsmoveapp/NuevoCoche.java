fd3917bf7a119eadf7ecb7631dc33215678f14a8
Francaste <francastefcb@gmail.com>
2019-04-19 22:47:45 +0200

    La funcionalidad de sacar y guardar foto en galería funciona
    Faltaban permisos de cámara y he cambiado cómo hacemos las fotos
    Hay que eliminar los métodos comentados

--- app/src/main/java/com/g9/letsmoveapp/NuevoCoche.java
+++ app/src/main/java/com/g9/letsmoveapp/NuevoCoche.java
@@ -1,12 +1,16 @@
 package com.g9.letsmoveapp;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.FileProvider;
@@ -28,12 +32,13 @@ import java.util.Date;
 
 public class NuevoCoche extends Activity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE =1001 ;
     Button button_camera;
     ImageView fotoCar;
     private final static String CLAVE_RUTA_IMAGEN = "CLAVE_RUTA_IMAGEN";
     public static final String LOG_TAG = "nuevoCoche";
-
-    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    Uri photoUri;//esta uri la guardamos en la base de datos en el registro de cada coche
 
 
     public void onCreate(Bundle savedInstanceState) {
@@ -42,21 +47,86 @@ public class NuevoCoche extends Activity {
         setContentView(R.layout.nuevo_coche);
 
         button_camera = findViewById(R.id.cam_car);
-        fotoCar=findViewById(R.id.foto_car);
+        fotoCar = findViewById(R.id.foto_car);
+
         button_camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //startActivity(takePictureIntent);
-                dispatchTakePictureIntent(takePictureIntent);
+                //ver permisos
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//check version
+                    if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        //dar permiso
+                        String[] permission = {android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else {
+                        //permission OK
+                        openCamera();
+                    }
+                } else {
+                    openCamera();
+                }
             }
         });
     }
+
+    private void openCamera() {
+        ContentValues values=new ContentValues();//creamos contenedor
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "ltsmvapp_"+ "_" + timeStamp ;//creamos un nombre para las fotos
+        values.put(MediaStore.Images.Media.TITLE,imageFileName);//añadimos nombre
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Let's MoveApp");//añadimos descripción
+        photoUri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);//insertamos en la uri externa titulo y descri
+
+        //Intent de la cámara
+        Intent photoIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//crear intent
+        photoIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);//poner extras
+        startActivityForResult(photoIntent, IMAGE_CAPTURE_CODE);//empezamos intent para obtener resultado
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK){//si se recibe result OK
+            Log.d(LOG_TAG, "photoUri: "+photoUri.toString());
+
+            fotoCar.setImageURI(photoUri);//mostramos en la ImageView la imagen guardada en la uri
+        }
+    }
+
+    public void add_car(View view) {
+        EditText editText = (EditText) findViewById(R.id.nombre_nuevo_coche);
+        String message = "Coche: " + editText.getText().toString() + " añadido correctamente";
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        finish();
+
+        // Cuando acaba una actividad vuelve a la actividad padre. No hace falta hacer un intent
+
+    }
+    @Override
+    //para los permisos
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+switch(requestCode){
+    case PERMISSION_CODE:{
+        if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            openCamera();
+        }else{
+            Toast.makeText(this,"No hay permiso para abrir cámara",Toast.LENGTH_LONG).show();
+        }
+    }
+}
+    }
+}
+/*
+
+*/
+/*
 /*
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nuevo_coche);
     }
-*/
+*//*
+
 
     public void add_car(View view) {
         EditText editText = (EditText) findViewById(R.id.nombre_nuevo_coche);
@@ -70,6 +140,7 @@ public class NuevoCoche extends Activity {
         //startActivity(intent);
     }
 
+*/
 /*en codelabs era private
   Este es el método que estaba ayer cuando la cámara funcionaba sin problemas
     public void intentCamara(View view) {
@@ -77,11 +148,13 @@ public class NuevoCoche extends Activity {
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-    }*/
+    }*//*
+
 
     //Este es el método que estaba ayer cuando la cámara funcionaba sin problemas pero no se usaba
     // de por sí en ningún sitio y ahora con la nueva, es el código que da problemas
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Log.d(LOG_TAG, "Data: "+data.toString());
             // Continue only if the File was successfully created
@@ -111,9 +184,15 @@ public class NuevoCoche extends Activity {
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
+                imageFileName,  */
+/* prefix *//*
+
+                ".jpg",         */
+/* suffix *//*
+
+                storageDir      */
+/* directory *//*
+
         );
 
         // Save a file: path for use with ACTION_VIEW intents
@@ -129,6 +208,7 @@ public class NuevoCoche extends Activity {
     //Nuevo
     private void dispatchTakePictureIntent(Intent takePictureIntent) {
         Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri photoURI =null;
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
@@ -142,13 +222,15 @@ public class NuevoCoche extends Activity {
             }
            // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
+                photoURI =
+                        FileProvider.getUriForFile(this,
                         "com.g9.letsmoveapp",
                         photoFile);
-                photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                //photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(photoIntent, REQUEST_TAKE_PHOTO);
             }
         }
     }
 
 }
+*/
